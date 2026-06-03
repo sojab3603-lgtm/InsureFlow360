e6274826c2931a2010122a8108cc8f2e9a71548d
Md Sojab <sojab@bourntec.com>
2026-06-03 14:16:26 +0530

    new files

---
+++ .sfdx/typings/lwc/apex/AgentforceInsuranceAiAssistantController.d.ts
@@ -0,0 +1,12 @@
+declare module "@salesforce/apex/AgentforceInsuranceAiAssistantController.getWelcomeMessage" {
+  export default function getWelcomeMessage(): Promise<any>;
+}
+declare module "@salesforce/apex/AgentforceInsuranceAiAssistantController.getClaimSummary" {
+  export default function getClaimSummary(): Promise<any>;
+}
+declare module "@salesforce/apex/AgentforceInsuranceAiAssistantController.getClaimsAnalytics" {
+  export default function getClaimsAnalytics(): Promise<any>;
+}
+declare module "@salesforce/apex/AgentforceInsuranceAiAssistantController.processUserQuery" {
+  export default function processUserQuery(param: {userQuery: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/apex/SecurityService.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/SecurityService.getUserSecurityContext" {
+  export default function getUserSecurityContext(): Promise<any>;
+}
